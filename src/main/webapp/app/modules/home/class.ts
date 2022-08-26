fb384422c20e3577329792cb10ab5b51a7039d28
Đông <dong.pd1994@outlook.com>
2022-08-26 17:11:40 +0700

    commit

---
+++ src/main/webapp/app/modules/home/class.ts
@@ -0,0 +1,23 @@
+import HIRAGANA from './hiragana.json';
+import KATAKANA from './katakana.json';
+import LESSON_1 from './lesson1/lesion';
+
+export const LESSON_JP = {
+  HIRAGANA_ABC: {
+    key: "hiragana",
+    title: "Hiragana",
+    source: HIRAGANA
+  },
+  KATAKANA_ABC: {
+    key: "katakana",
+    title: "Katakana",
+    source: KATAKANA
+  },
+  LESSION_ONE: {
+    key: "lesson1",
+    title: "Lesson 1",
+    source: LESSON_1
+  }
+}
+
+export default LESSON_JP;
---
+++ src/main/webapp/app/modules/home/lesson1/lesion.ts
@@ -0,0 +1,10 @@
+import NEW_WORD from './new-word.json'
+import NEW_SENTENCE from './new-sentence.json'
+import OTHER from './other.json'
+import _ from 'lodash'
+
+
+
+export const LESSON = _.assign(NEW_WORD, NEW_SENTENCE, OTHER)
+
+export default LESSON

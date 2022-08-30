a67445703448720e287eab44a2fa859a5ade5022
Pham Dong Dong <dong.pd1994@outlook.com>
2022-08-30 17:00:05 +0700

    commit

--- src/main/webapp/app/modules/home/lesson1/lesion.ts
+++
@@ -1,8 +0,0 @@
-import NEW_WORD from './new-word.json'
-import NEW_SENTENCE from './new-sentence.json'
-import OTHER from './other.json'
-import _ from 'lodash'
-
-export const LESSON1 = _.assign(NEW_WORD, NEW_SENTENCE, OTHER)
-
-export default LESSON1
